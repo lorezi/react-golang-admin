89a4bad5124bed9d9bb1451704ea6c9ab9b873b8
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-04-10 11:23:50 +0100

    dashboard and user page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Menu from "./components/Menu";
 import Nav from "./components/Nav";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -11,28 +12,8 @@ function App() {
           <Menu />
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            {/* <Users /> */}
+            <Dashboard />
           </main>
         </div>
       </div>
