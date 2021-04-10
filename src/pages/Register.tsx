f5b0c427131ba6c6198ad910399c086eb713c9c9
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-04-10 16:35:37 +0100

    register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,72 @@
-import React, { Component } from "react";
+import React, { Component, SyntheticEvent } from "react";
+import "../Login.css";
 
 export default class Register extends Component {
+  first_name = "";
+  last_name = "";
+  email = "";
+  password = "";
+  password_confirm = "";
+
+  submit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log({
+      first_name: this.first_name,
+      last_name: this.last_name,
+      email: this.email,
+      password: this.password,
+      password_confirm: this.password_confirm,
+    });
+  };
+
   render() {
-    return <div>Register Page</div>;
+    return (
+      <main className="form-signin">
+        <form onSubmit={this.submit}>
+          <h1 className="h3 mb-3 fw-normal">Please Register</h1>
+
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First Name"
+            required
+            onChange={(e) => (this.first_name = e.target.value)}
+          />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Last Name"
+            required
+            onChange={(e) => (this.last_name = e.target.value)}
+          />
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email"
+            required
+            onChange={(e) => (this.email = e.target.value)}
+          />
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            required
+            onChange={(e) => (this.password = e.target.value)}
+          />
+
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password Confirm"
+            required
+            onChange={(e) => (this.password_confirm = e.target.value)}
+          />
+
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Register
+          </button>
+        </form>
+      </main>
+    );
   }
 }
