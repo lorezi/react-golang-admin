5a04959dc3382a846e162dc7970641dc0b781f4a
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-04-10 11:26:52 +0100

    routes for dashboard and user

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Menu from "./components/Menu";
 import Nav from "./components/Nav";
 import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
 
 function App() {
   return (
@@ -12,8 +14,10 @@ function App() {
           <Menu />
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            {/* <Users /> */}
-            <Dashboard />
+            <BrowserRouter>
+              <Route path={"/"} component={Dashboard} />
+              <Route path={"/users"} component={Users} />
+            </BrowserRouter>
           </main>
         </div>
       </div>
