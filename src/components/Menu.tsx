1bef3e4f83fda71c95347ead7b4e1b93f6788edb
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-04-12 10:42:01 +0100

    active class menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
@@ -10,16 +10,16 @@ const Menu = () => {
       <div className="position-sticky pt-3">
         <ul className="nav flex-column">
           <li className="nav-item">
-            <Link to={"/"} className="nav-link">
+            <NavLink to={"/"} exact className="nav-link">
               <span data-feather="home"></span>
               Dashboard
-            </Link>
+            </NavLink>
           </li>
 
           <li className="nav-item">
-            <Link to={"/users"} className="nav-link">
+            <NavLink to={"/users"} className="nav-link">
               Users
-            </Link>
+            </NavLink>
           </li>
         </ul>
       </div>
