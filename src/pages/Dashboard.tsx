06ae6c42bfce225824fcdf6f1fe8b45aa0074471
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-04-14 17:27:43 +0100

    dashboard and chart

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,48 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import Wrapper from "../components/Wrapper";
+import * as c3 from "c3";
+import axios from "axios";
 
-export default class Dashboard extends Component {
-  render() {
-    return <Wrapper>Dashboard</Wrapper>;
-  }
-}
+const Dashboard = () => {
+  useEffect(() => {
+    (async () => {
+      const chart = c3.generate({
+        bindto: "#chart",
+        data: {
+          x: "x",
+          columns: [["x"], ["Sales"]],
+          types: {
+            Sales: "bar",
+          },
+        },
+
+        axis: {
+          x: {
+            type: "timeseries",
+            tick: {
+              format: "%Y-%m-%d",
+            },
+          },
+        },
+      });
+
+      const { data } = await axios.get("chart");
+      chart.load({
+        columns: [
+          ["x", ...data.map((res: any) => res.date)],
+          ["Sales", ...data.map((res: any) => res.sum)],
+        ],
+      });
+    })();
+    return () => {};
+  }, []);
+  return (
+    <Wrapper>
+      <h2>Daily Sales</h2>
+
+      <div className="" id="chart" />
+    </Wrapper>
+  );
+};
+
+export default Dashboard;
